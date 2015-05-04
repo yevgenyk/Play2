a75c92978484c77e00e5690c2a28927ce30b8d12
Kolyakov, Yevgeny <yevgeny.kolyakov@teamaol.com>
2015-05-04 13:20:34 -0400

    tweaks

--- subs.sh
+++ subs.sh
@@ -1,8 +1,3 @@
-git rm -rf deps/optional
-git rm -rf deps/djinni
-git rm -rf deps/json11
-git rm -rf deps/SQLiteCpp
-
 git submodule add https://github.com/akrzemi1/Optional.git deps/optional
 git submodule add https://github.com/dropbox/djinni.git deps/djinni
 git submodule add https://github.com/libmx3/json11.git deps/json11
